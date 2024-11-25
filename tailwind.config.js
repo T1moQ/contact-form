36c8da0e376d733feeb228c626a630a281322b74
Timofey <pttima@yandex.ru>
2024-11-25 08:23:55 +0300

    tailwindcss init. tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = {
+	primary: {
+		'green-200': ' hsl(148, 38%, 91%)',
+		'green-600': ' hsl(169, 82%, 27%)',
+		red: '  hsl(0, 66%, 54%)',
+	},
+	neutral: {
+		'grey-500': 'hsl(186, 15%, 59%)',
+		'grey-900': 'hsl(169, 82%, 27%)',
+		'grey-500': 'hsl(0, 66%, 54%)',
+	},
+}
+
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		colors,
+		screens: {
+			sm: '375px',
+			lg: '1440px',
+		},
+		fontSize: {
+			labels: '16px',
+		},
+		fontFamily: {
+			sans: ['Karla', 'sans-serif'],
+		},
+	},
+	plugins: [],
+}
